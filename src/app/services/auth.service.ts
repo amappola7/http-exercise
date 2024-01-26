d30a85dc3922d6b968310fb41fcb3340b56f91db
Amappola <anam.porrasp@gmail.com>
2024-01-26 11:54:25 -0500

    Creating token methods

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -19,4 +19,24 @@ export class AuthService {
   loginUser(userCredentials: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/login`, userCredentials)
   }
+
+  // Métodos para almacenar y obtener el token de acceso
+  private setAccessToken(token: string): void {
+    localStorage.setItem('access_token', token);
+  }
+
+  private getAccessToken(): string | null {
+    return localStorage.getItem('access_token');
+  }
+
+  // Método para validar si el usuario está autenticado o no
+  isAuthenticated(): boolean {
+    const token = this.getAccessToken();
+    return !!token; // Devuelve true si hay un token, lo que significa que el usuario está autenticado
+  }
+
+  // Método para cerrar sesión (simulado)
+  logout(): void {
+    localStorage.removeItem('access_token');
+  }
 }
