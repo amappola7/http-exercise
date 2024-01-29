19cc2bf5207e176548d2bb705d7c352d0fbaeb62
Daniela <yudadaniela@hotmail.com>
2024-01-29 09:15:04 -0500

    httpFail

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,24 @@ export class ProductService {
   private apiUrl = 'https://fakeUrl.com/products';
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private authService:AuthService
   ) { }
 
+  getProduct():Observable<any>{
+    if(this.authService.isAuthenticated()){
+      const token=localStorage.getItem('acces_token')
+      const headers= new HttpHeaders({
+        'content-type':'application/json',
+        'Authorization':`Bearer ${token}`
+      });
+      return this.http.get(`${this.apiUrl}`, {headers:headers})
+    }else{
+      return new Observable()
+    }
+    
+
+  }
   createProduct(productData: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/create`, productData)
   }
