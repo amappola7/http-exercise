31341de92c1b4a2d1114388766918f89c03bb6a2
Amappola <anam.porrasp@gmail.com>
2024-01-29 08:56:18 -0500

    Getting products

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,36 @@ export class ProductService {
   private apiUrl = 'https://fakeUrl.com/products';
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private authService: AuthService
   ) { }
 
+  getProducts(): Observable<any> {
+    if (this.authService.isAuthenticated()) {
+      const token = localStorage.getItem('access_token');
+      const headers = new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      });
+      return this.http.get(`${this.apiUrl}`, { headers: headers })
+
+    } else {
+      return new Observable();
+    }
+  }
+
   createProduct(productData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/create`, productData)
+    if (this.authService.isAuthenticated()) {
+      const token = localStorage.getItem('access_token');
+      const headers = new HttpHeaders({
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      });
+      return this.http.post(`${this.apiUrl}/create`, productData, { headers: headers })
+
+    } else {
+      return new Observable();
+    }
+
   }
 }
